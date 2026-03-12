34a80f3fa43f066e729f697489dccf09fedda063
madhu1217 <bathula.madhu999@gmail.com>
2026-03-13 01:39:41 +0530

    ingress fix back service_

--- Application-Code/frontend/src/services/taskServices.js
+++ Application-Code/frontend/src/services/taskServices.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 // const apiUrl = process.env.REACT_APP_BACKEND_URL
-const apiUrl = http://35.170.147.51:30467/api/tasks
-console.log(apiUrl)
+const apiUrl = "/api/tasks";
+console.log(apiUrl);
 export function getTasks() {
-    return axios.get(apiUrl);
+  return axios.get(apiUrl);
 }
 
 export function addTask(task) {
-    return axios.post(apiUrl, task);
+  return axios.post(apiUrl, task);
 }
 
 export function updateTask(id, task) {
-    return axios.put(apiUrl + "/" + id, task);
+  return axios.put(apiUrl + "/" + id, task);
 }
 
 export function deleteTask(id) {
-    return axios.delete(apiUrl + "/" + id);
+  return axios.delete(apiUrl + "/" + id);
 }
