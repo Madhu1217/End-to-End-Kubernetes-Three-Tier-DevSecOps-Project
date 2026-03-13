cc42bb6b5dac0b7073715450093d7a9c7a94373c
madhu1217 <bathula.madhu999@gmail.com>
2026-03-13 13:18:31 +0530

    fixw bak service_2

--- Application-Code/frontend/src/services/taskServices.js
+++ Application-Code/frontend/src/services/taskServices.js
@@ -1,8 +1,31 @@
+// import axios from "axios";
+// // REACT_APP_BACKEND_URL=http://35.170.147.51:30008/api/tasks
+// const apiUrl = process.env.REACT_APP_BACKEND_URL || "/api/tasks";
+// // const apiUrl = "/api/tasks";
+// // const apiUrl = "http://35.170.147.51:30008/api/tasks";
+// console.log(apiUrl);
+// export function getTasks() {
+//   return axios.get(apiUrl);
+// }
+
+// export function addTask(task) {
+//   return axios.post(apiUrl, task);
+// }
+
+// export function updateTask(id, task) {
+//   return axios.put(apiUrl + "/" + id, task);
+// }
+
+// export function deleteTask(id) {
+//   return axios.delete(apiUrl + "/" + id);
+// }
+
+// // check
+
 import axios from "axios";
-REACT_APP_BACKEND_URL=http://35.170.147.51:30008/api/tasks
-const apiUrl = process.env.REACT_APP_BACKEND_URL || "/api/tasks";
-// const apiUrl = "/api/tasks";
-console.log(apiUrl);
+
+const apiUrl = "/api/tasks";
+
 export function getTasks() {
   return axios.get(apiUrl);
 }
@@ -12,11 +35,9 @@ export function addTask(task) {
 }
 
 export function updateTask(id, task) {
-  return axios.put(apiUrl + "/" + id, task);
+  return axios.put(`${apiUrl}/${id}`, task);
 }
 
 export function deleteTask(id) {
-  return axios.delete(apiUrl + "/" + id);
+  return axios.delete(`${apiUrl}/${id}`);
 }
-
-// check
